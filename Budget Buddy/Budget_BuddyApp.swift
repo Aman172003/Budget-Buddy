03daa8abb7841002710f451de0147b20bf2136b2
Aman Kumar Rai <tej171995@gmail.com>
2024-04-26 08:42:16 +0530

    Initial Commit

---
+++ Budget Buddy/Budget_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Budget_BuddyApp.swift
+//  Budget Buddy
+//
+//  Created by Aman on 26/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Budget_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Budget Buddy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Budget Buddy
+//
+//  Created by Aman on 26/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
